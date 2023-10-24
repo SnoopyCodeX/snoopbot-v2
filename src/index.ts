28f195bb0c91d86081de033e34c2ed894086da46
SnoopyCodeX <extremeclasherph@gmail.com>
2023-10-24 20:06:12 +0800

    Refactored event listening

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import SnoopBot from "./core/snoopbot"
 import * as commands from "./commands"
+import * as events from "./events"
 
 const bot = new SnoopBot()
 bot.init()
+
+bot.on('gc:member_join', new events.MemberJoinEvent())
+bot.on('gc:member_leave', new events.MemberLeaveEvent())
+bot.on('message:unsend', new events.MessageUnsendEvent())
 
 bot.addCommand(new commands.HelpCommand())
