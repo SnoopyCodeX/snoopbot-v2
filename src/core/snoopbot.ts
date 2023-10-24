89eb1fe15249bcbb93411b7aedcf0b82da34571a
SnoopyCodeX <extremeclasherph@gmail.com>
2023-10-24 20:08:18 +0800

    Refactored SnoopBot class to support class-based events

--- src/core/snoopbot.ts
+++ src/core/snoopbot.ts
@@ -11,8 +11,8 @@ dotenv.config()
 
 export default class SnoopBot {
     private commands: Array<Command> = []
+    private events: SnoopBotThreadEvent = {}
     private commandMiddlewares: Array<Function> = []
-    private eventMiddlewares: Array<Function> = []
     private appstate: String = '[]'
     private queue: Queue = new Queue(1, "GlobalQueue")
 
@@ -34,26 +34,64 @@ export default class SnoopBot {
         userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML; like Gecko) Version/8.0.3 Safari/600.3.18"
     }
 
+    private getCommandsOptions() : Array<SnoopBotCommandOptions> {
+        return this.commands.map((command) => command.options)
+    }
+    
+    /**
+     * Creates a new SnoopBot instance
+     * and loads facebook appstate from
+     * environment variables
+     */
     public constructor() {
         this.appstate = process.env.APPSTATE ?? '[]'
     }
 
+    /**
+     * Adds new command that SnoopBot will
+     * execute.
+     * 
+     * @param command The command to be added to SnoopBot
+     */
     public addCommand(command: Command) : void {
         this.commands.push(command);
     }
 
-    private getCommandsOptions() : Array<SnoopBotCommandOptions> {
-        return this.commands.map((command) => command.options)
-    }
-
+    /**
+     * Adds a new command middleware. This middleware will be
+     * the first executed before executing the actual commands
+     * registered in SnoopBot.
+     * 
+     * @param commandMiddleware Command middleware to be added
+     */
     public addCommandMiddleware(...commandMiddleware: Array<Function>) : void {
         this.commandMiddlewares.push(...commandMiddleware)
     }
 
-    public addEventMiddleware(...eventMiddleware: Array<Function>) : void {
-        this.eventMiddlewares.push(...eventMiddleware)
+    /**
+     * Listens to thread events that are triggered
+     * on specific actions in a thread
+     * 
+     * @param eventType Type of event to listen to. Refer to `SnoopBotEventType` for type of supported events.
+     * @param event Class that extends `SnoopBotEvent`, its method `SnoopBotEvent::onEvent()` will be executed when the event it's binded to is triggered.
+     * @returns void
+     */
+    public on(eventType: SnoopBotEventType, event: SnoopBotEvent) : void {
+        if(this.events[eventType] !== undefined) {
+            Logger.error(`The event ${eventType} is already being listened to!`)
+            return
+        }
+
+        this.events[eventType] = event
     }
 
+    /**
+     * Initializes SnoopBot and
+     * logs in to facebook using the provided appstate
+     * in the environment variable.
+     * 
+     * @param options Options for SnoopBot. See `SnoopBotOptions` for supported options.
+     */
     public init(options: SnoopBotOptions = this.options) : void {
         this.options = {
             ...this.options,
@@ -84,8 +122,69 @@ export default class SnoopBot {
 
                     process.env.APPSTATE = JSON.stringify(api.getAppState())
 
-                    const eventCallback = () => async(event: any, api: any) => {}
-                    global.eventsQueue.enqueue(async() => await pipeline([...this.eventMiddlewares, eventCallback], event, api))
+                    if(event.type === 'event') {
+                        let thread = await api.getThreadInfo(event.threadID)
+                        let eventType = event.logMessageType
+
+                        switch(eventType) {
+                            // Member joined a group chat
+                            case 'log:subscribe':
+                                // Do nothing if the thread isn't a group chat
+                                if(!thread.isGroup) break
+
+                                // Execute the callback if it exists in the events array
+                                if(this.events['gc:member_join'] !== undefined)
+                                    this.queue.enqueue(async() => await this.events['gc:member_join'].onEvent(event, api))
+                            break
+
+                            // Member left a group chat
+                            case 'log:unsubscribe':
+                                // Do nothing if the thread isn't a group chat
+                                if(!thread.isGroup) break
+
+                                // Execute the callback if it exists in the events array
+                                if(this.events['gc:member_leave'] !== undefined)
+                                    this.queue.enqueue(async() => await this.events['gc:member_leave'].onEvent(event, api))
+                            break
+
+                            // A group chat changed name
+                            case 'log:thread-name':
+                                // Do nothing if the thread isn't a group chat
+                                if(!thread.isGroup) break
+
+                                // Execute the callback if it exists in the events array
+                                if(this.events['gc:change_name'] !== undefined)
+                                    this.queue.enqueue(async() => await this.events['gc:change_name'].onEvent(event, api))
+                            break
+                            
+                            // A group chat changed icon
+                            case 'log:thread-icon':
+                                // Do nothing if the thread isn't a group chat
+                                if(!thread.isGroup) break
+
+                                // Execute the callback if it exists in the events array
+                                if(this.events['gc:change_icon'] !== undefined)
+                                    this.queue.enqueue(async() => await this.events['gc:change_icon'].onEvent(event, api))
+                            break
+
+                            // A group chat changed theme
+                            case 'log:thread-color':
+                                // Do nothing if the thread isn't a group chat
+                                if(!thread.isGroup) break
+
+                                // Execute the callback if it exists in the events array
+                                if(this.events['gc:change_theme'] !== undefined)
+                                    this.queue.enqueue(async() => await this.events['gc:change_theme'].onEvent(event, api))
+                            break
+
+                            // A user changed nickname
+                            case 'log:user-nickname':
+                                // Execute the callback if it exists in the events array
+                                if(this.events['user:change_nickname'] !== undefined)
+                                    this.queue.enqueue(async() => await this.events['user:change_nickname'].onEvent(event, api))
+                            break
+                        }
+                    }
 
                     settings = Settings.getSettings()
                     const threadSettings = settings.threads[event.threadID] || settings.defaultSettings
