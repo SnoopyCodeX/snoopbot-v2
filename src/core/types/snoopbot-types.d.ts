8d8afe9a94333c86a418f270f389450e20cc425f
SnoopyCodeX <extremeclasherph@gmail.com>
2023-10-24 20:08:54 +0800

    Implemented SnoopBotEventType and SnoopBotThreadEvent

--- src/core/types/snoopbot-types.d.ts
+++ src/core/types/snoopbot-types.d.ts
@@ -39,3 +39,28 @@ type SnoopBotCommandExtras = {
     commands: SnoopBotCommandOptions[];
     global: any;
 }
+
+/**
+ * Types of events that are triggered
+ * in a conversation/thread
+ * 
+ * @event gc:member_join Triggered when a user joins a group chat
+ * @event gc:member_join Triggered when a user leaves a group chat
+ * @event gc:change_icon Triggered when a group chat changes its icon
+ * @event gc:change_theme Triggered when a group chat changes its theme
+ * @event gc:change_name Triggered when a group chat changes its name
+ * @event user:change_nickname Triggered when a user changes its nickname
+ * @event message:unsent Triggered when a message is unsent
+ */
+type SnoopBotEventType = 
+     'gc:member_join' |
+     'gc:member_leave' |
+     'gc:change_icon' | 
+     'gc:change_theme' | 
+     'gc:change_name' |
+     'user:change_nickname' | 
+     'message:unsend'
+
+type SnoopBotThreadEvent = {
+    [key: string]: SnoopBotEvent;
+}
