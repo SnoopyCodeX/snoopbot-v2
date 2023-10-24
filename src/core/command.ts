50fe1e44456a4b259b2eb7d128ccd39ebd9c93af
SnoopyCodeX <extremeclasherph@gmail.com>
2023-10-24 20:07:51 +0800

    Refactored base class for snoopbot commands

--- src/core/command.ts
+++ src/core/command.ts
@@ -1,4 +1,4 @@
-export default class Command {
+export default class SnoopBotCommand {
     public options: SnoopBotCommandOptions
 
     constructor(options: SnoopBotCommandOptions) {
@@ -6,6 +6,6 @@ export default class Command {
     }
 
     public async execute(matches: Array<any>, event: any, api: any, extras: SnoopBotCommandExtras) : Promise<void> {
-        throw new Error("Command::execute() is unimplmented")
+        throw new Error("SnoopBotCommand::execute() is unimplmented")
     }
 }
