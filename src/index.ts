edef2e06c4777ec4ede522edcad20c855666e902
SnoopyCodeX <extremeclasherph@gmail.com>
2023-10-31 19:49:34 +0800

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import * as commands from "./commands"
 import * as events from "./events"
 
 const bot = new SnoopBot()
-bot.init()
+bot.init({
+    selfListen: true
+})
 
 bot.on('gc:member_join', new events.MemberJoinEvent())
 bot.on('gc:member_leave', new events.MemberLeaveEvent())
 bot.on('message:unsend', new events.MessageUnsendEvent())
 
 bot.addCommand(new commands.HelpCommand())
+bot.addCommand(new commands.SenderInfoCommand())
