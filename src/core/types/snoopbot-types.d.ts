643006ee92ae6cc55f1fc9d12b45e0feedfae415
SnoopyCodeX <extremeclasherph@gmail.com>
2023-10-31 19:52:40 +0800

    Refactored snoopbot-types.ts

--- src/core/types/snoopbot-types.d.ts
+++ src/core/types/snoopbot-types.d.ts
@@ -1,19 +1,19 @@
 type SnoopBotOptions = {
-    configs: Object = {};
-    handleMatches: boolean = true;
+    configs?: Object = {};
+    handleMatches?: boolean = true;
     
-    selfListen: boolean = false;
-    listenEvents: boolean = false;
-    listenTyping: boolean = false;
-    updatePresence: boolean = false;
-    forceLogin: boolean = false;
-    autoMarkDelivery: boolean = true;
-    autoMarkRead: boolean = false;
-    autoReconnect: boolean = true;
-    logRecordSize: number = 100;
-    online: boolean = true;
-    emitReady: boolean = false;
-    userAgent: string = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML; like Gecko) Version/8.0.3 Safari/600.3.18";
+    selfListen?: boolean = false;
+    listenEvents?: boolean = false;
+    listenTyping?: boolean = false;
+    updatePresence?: boolean = false;
+    forceLogin?: boolean = false;
+    autoMarkDelivery?: boolean = true;
+    autoMarkRead?: boolean = false;
+    autoReconnect?: boolean = true;
+    logRecordSize?: number = 100;
+    online?: boolean = true;
+    emitReady?: boolean = false;
+    userAgent?: string = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML; like Gecko) Version/8.0.3 Safari/600.3.18";
 }
 
 type SnoopBotCommandOptions = {
