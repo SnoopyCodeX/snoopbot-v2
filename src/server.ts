0fc6eaa0e739b75af5deeb1f45f359dcbc1d7e7c
SnoopyCodeX <extremeclasherph@gmail.com>
2023-11-23 21:12:40 +0800

    Added src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import { Logger } from "./snoopbot";
+import axios from "axios";
 const express = require('express');
 
 // Initialize express
@@ -8,5 +10,10 @@ app.get('/', (req: any, res: any) => {
     res.send("Hello world");
 });
 
+Logger.success("🚀Server running on port 3000!");
+
 // Listen to port 3000
 app.listen(3000);
+
+// Load the server every 1.5 seconds to prevent from hibernating
+setInterval(() => axios.get("http://localhost:3000/").then((response) => Logger.muted(`Server: ${response.data}`)), 1500);
