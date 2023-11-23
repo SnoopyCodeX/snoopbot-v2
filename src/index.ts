71595e8e68236eab5163d6ba7ff7b497fab652df
SnoopyCodeX <extremeclasherph@gmail.com>
2023-11-23 21:12:22 +0800

    Refactored src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@ import { SnoopBot } from "./snoopbot"
 import * as commands from "./commands"
 import * as events from "./events"
 
+// Initialize SnoopBot
 const bot = new SnoopBot()
 bot.init({
     selfListen: true,
     debugMode: true,
 })
 
+// Event bindings
 bot.on('gc:member_join', new events.MemberJoinEvent())
 bot.on('gc:member_leave', new events.MemberLeaveEvent())
 bot.on('message:unsend', new events.MessageUnsendEvent())
 
+// Regular commands
 bot.addCommand(new commands.HelpCommand())
 bot.addCommand(new commands.PlayCommand())
 bot.addCommand(new commands.ReverseImageSearchCommand())
