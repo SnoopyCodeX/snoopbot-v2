67814fc80d7eaf71c3f2b291379192146fabd1d2
SnoopyCodeX <extremeclasherph@gmail.com>
2023-11-26 20:42:22 +0800

    Added new commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,9 @@
 export { default as HelpCommand } from './help'
 export { default as PlayCommand } from './play'
 export { default as ReverseImageSearchCommand } from './ris'
+export { default as QRCodeCommand } from './qr'
+
 export { default as PermissionCommand } from './permission'
 export { default as JoinOrLeaveCommand } from './joinOrLeave'
+export { default as AdminCommand } from './admin'
+export { default as SettingsCommand } from './settings'
