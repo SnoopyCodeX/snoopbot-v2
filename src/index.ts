caecd0fd13fb834f4ff161c9f7e32829c486ee1c
SnoopyCodeX <extremeclasherph@gmail.com>
2023-12-09 21:20:35 +0800

    Removed manual importing of commands, events and middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import 'module-alias/register'
 import { SnoopBot } from "@snoopbot"
-import * as commands from "@commands"
-import * as middlewares from "@middlewares"
-import * as events from "@events"
 
 // Initialize SnoopBot
 const bot = new SnoopBot()
@@ -10,26 +7,3 @@ bot.init({
     selfListen: true,
     debugMode: true,
 })
-
-// Middlewares
-bot.addCommandMiddleware(
-    new middlewares.JoinOrLeaveMiddleware(),
-    new middlewares.PermissionMiddleware(),
-)
-
-// Event bindings
-bot.on('gc:member_join', new events.MemberJoinEvent())
-bot.on('gc:member_leave', new events.MemberLeaveEvent())
-bot.on('message:unsend', new events.MessageUnsendEvent())
-
-// Regular commands
-bot.addCommand(new commands.HelpCommand())
-bot.addCommand(new commands.PlayCommand())
-bot.addCommand(new commands.ReverseImageSearchCommand())
-bot.addCommand(new commands.QRCodeCommand())
-
-// Admin commands
-bot.addCommand(new commands.AdminCommand({ adminOnly: true }))
-bot.addCommand(new commands.JoinOrLeaveCommand({ adminOnly: true }))
-bot.addCommand(new commands.PermissionCommand({ adminOnly: true }))
-bot.addCommand(new commands.SettingsCommand({ adminOnly: true }))
